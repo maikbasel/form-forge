cf2efa51e41a260cae0ef0770918f206ab1f7f38
Maik Basel <maik.basel@gmx.de>
2025-08-08 15:36:46 +0200

    refactor: simplify HttpServer initialization in main function

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use actix_web::{App, HttpServer};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let addr = std::env::var("BIND_ADDR").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
-    HttpServer::new(|| App::new()).bind(addr)?.run().await
+    HttpServer::new(App::new).bind(addr)?.run().await
 }
