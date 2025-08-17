1bca81161158b8d44e38b0a65b0342b77d222fb0
Maik Basel <maik.basel@gmx.de>
2025-08-17 21:41:50 +0200

    test: add test for sheet download

--- tests/test_sheet_upload.rs
+++ tests/test_sheet_upload.rs
@@ -10,7 +10,7 @@ mod tests {
     use uuid::Uuid;
 
     #[actix_web::test]
-    async fn test_should_store_sheet_and_mapping() {
+    async fn test_should_upload_sheet_and_binding() {
         let app_config = AppConfig::initialize()
             .await
             .expect("initialize app config");
@@ -31,9 +31,38 @@ mod tests {
             .headers()
             .get(header::LOCATION)
             .expect("Location header is present");
-        let location = location.to_str().expect("Location header is valid");
+        let location = location.to_str().expect("location header is valid");
         assert_eq!(location.starts_with("/sheets/"), true);
-        let id_part = location.split("/").last().expect("Location header has id");
+        let id_part = location.split("/").last().expect("location header has id");
         assert!(Uuid::parse_str(id_part).is_ok());
+    }
+
+    #[actix_web::test]
+    async fn test_should_download_sheet() {
+        let app_config = AppConfig::initialize()
+            .await
+            .expect("initialize app config");
+        telemetry::initialize().expect("initialize telemetry");
+        let app = test_utils::app!(app_config);
+        let (header, body) = test_utils::dnd_multipart_form_data().build();
+        let upload_req = test::TestRequest::post()
+            .uri("/sheets")
+            .insert_header(header)
+            .set_payload(body)
+            .to_request();
+        let upload_resp = test::call_service(&app, upload_req).await;
+        let location = upload_resp
+            .headers()
+            .get(header::LOCATION)
+            .expect("location header is present");
+        let location = location.to_str().expect("location header is valid");
+        let req = test::TestRequest::get().uri(location).to_request();
+
+        let resp = test::call_service(&app, req).await;
+
+        assert_eq!(resp.status(), StatusCode::OK);
+        let body_bytes = test::read_body(resp).await;
+        assert!(!body_bytes.is_empty());
+        assert!(body_bytes.starts_with(b"%PDF-"));
     }
 }
