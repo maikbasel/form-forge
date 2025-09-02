7eaf622572ae81c914954ae58beb871cf0faccab
Maik Basel <maik.basel@gmx.de>
2025-09-02 21:35:20 +0200

    refactor: remove unused download_sheet route from web adapter

--- sheets/adapters/web/src/lib.rs
+++ sheets/adapters/web/src/lib.rs
@@ -3,6 +3,5 @@ use actix_web::web;
 mod handler;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.route("/sheets", web::post().to(handler::upload_sheet))
-        .route("/sheets/{id}", web::get().to(handler::download_sheet));
+    cfg.route("/sheets", web::post().to(handler::upload_sheet));
 }
